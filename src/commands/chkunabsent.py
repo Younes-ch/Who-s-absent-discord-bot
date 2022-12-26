536526e65faca8f05b2281044f4b4b9c733f61b8
Younes-ch <younes.jmell10@gmail.com>
2022-12-26 21:51:48 +0100

    Merged commands to slash commands.

---
+++ src/commands/chkunabsent.py
@@ -0,0 +1,47 @@
+from discord.ext import commands
+from discord import app_commands
+import discord
+
+class Chkunabsent(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(description="Tchuf les membres eli aandhum el role mawjoudin fil voice channel wela lé")
+    @app_commands.checks.has_any_role("Leads", "Younes")
+    async def chkunabsent(self, interaction: discord.Interaction, role: discord.Role):
+        if interaction.user.voice is None:
+            await interaction.response.send_message(f"{interaction.user.mention} Lezim tabda fi voice channel!", ephemeral=True)
+            return
+        else:
+            chkun_absent: list[discord.Member] = []
+            to_be_instructed: list[discord.Member] = []
+            for member in role.members:
+                if not member.bot:
+                    if not member.voice:
+                        chkun_absent.append(member)
+                    elif member.voice.channel != interaction.user.voice.channel:
+                        to_be_instructed.append(member)
+            if to_be_instructed:
+                await interaction.channel.send(f'{", ".join(map(lambda x: x.mention, to_be_instructed))} arwa7 lil channel hedhy {interaction.user.voice.channel.mention}')
+            if len(chkun_absent) == 0:
+                await interaction.response.send_message("Lkol hadhrin!")
+            else:
+                msg = ""
+                for member in chkun_absent:
+                    msg += member.mention + ", "
+                if len(chkun_absent) == 1:
+                    await interaction.response.send_message(content=f"{msg[:-2]} bech t7el el cam ki tji w ezreb ru7yk la njik bchleka.")
+                else:
+                    await interaction.response.send_message(content=f"{msg[:-2]}\nbech t7elu el cam ki tjiw w ezrebu rwe7kum la njikum bchleka.")
+
+    @chkunabsent.error
+    async def chkunabsent_error(self, interaction: discord.Interaction, error: Exception):
+        if isinstance(error, app_commands.MissingAnyRole):
+            await interaction.response.send_message("allah ghaleb sala7iyetik ma7douda. 😏", ephemeral=True)
+        else:
+            embed = discord.Embed(description=":no_entry: An error occured while executing this command. Please try again later.", color=0xff0000)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            print(error)
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Chkunabsent(bot))
--- src/main.py
+++ src/main.py
@@ -1,55 +1,28 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import logging
 import os
 
+class Bot(commands.Bot):
+  def __init__(self):
+    super().__init__(command_prefix="?", intents=discord.Intents.all(), activity=discord.Activity(type=discord.ActivityType.listening, name="/chkunabsent"))
+    self.available_commands = ["commands.chkunabsent"]
+
+  async def on_ready(self):
+    print("Ready!")
+    try:
+      synced = await self.tree.sync()
+      print(f"Synced {len(synced)} command(s):")
+      for command in synced:
+          print('\033[33m', command.name, '\33[0m-->', command.description)
+    except Exception as e:
+      print(e)
+
+  async def setup_hook(self):
+    for extension in self.available_commands:
+      await self.load_extension(extension)
+      print(f"Loaded \033[33m{extension.replace('commands.', '')}.py\033[0m")
+
+bot = Bot()
 load_dotenv()
-intents = discord.Intents.all()
-activity = discord.Activity(type=discord.ActivityType.listening, name="&chkunabsent")
-logging.basicConfig(level=logging.INFO)
-bot = commands.Bot(command_prefix='&', intents=intents)
-
-
-@bot.event
-async def on_ready():
-  print("Ready!")
-
-@bot.command()
-@commands.has_any_role("Leads", "Younes")
-async def chkunabsent(ctx, *, role : discord.Role):
-  if ctx.author.voice == None:
-    await ctx.send(f"{ctx.author.mention} Lezim tabda fi voice channel!")
-  else:
-    chkun_absent = []
-    for member in role.members:
-      if not member.voice:
-        chkun_absent.append(member)
-      elif member.voice.channel != ctx.author.voice.channel:
-        await ctx.send(f"{member.mention} arwa7 lil channel hedhy {ctx.author.voice.channel.mention}")
-    print(chkun_absent)
-    if len(chkun_absent) == 0:
-      await ctx.send("Lkol hadhrin!")
-    else:
-      msg = ""
-      for member in chkun_absent:
-        msg += member.mention + ", "
-      if len(chkun_absent) == 1:
-        await ctx.send(f"{msg[:-2]} bech t7el el cam ki tji w ezreb ru7yk la njik bchleka.")
-      else:
-        await ctx.send(f"{msg[:-2]}\nbech t7elu el cam ki tjiw w ezrebu rwe7kum la njikum bchleka.")
-
-@chkunabsent.error
-async def chkunabsent_error(ctx, error : commands.CommandError):
-  print(error)
-  if isinstance(error, commands.RoleNotFound):
-    await ctx.message.add_reaction("❌")
-    await ctx.send("Role mahuch mawjud manaarch mnin tlaat bih. 🤔")
-  elif isinstance(error, commands.CheckFailure):
-    await ctx.message.add_reaction("❌")
-    await ctx.send("allah ghaleb sala7iyetik ma7douda. 😏")
-  else:
-    await ctx.message.add_reaction("❌")
-    await ctx.send("Zidna el role yarhem bouk! 🤦")
-
-bot.run(os.environ["TOKEN"])
+bot.run(os.getenv("TOKEN"))
